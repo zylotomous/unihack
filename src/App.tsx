0d25141fe0be05b27d4b45188fc610894c08c2d3
Ddrewy <deandrew.y@gmail.com>
2026-03-14 16:06:24 +1100

    updateL udpating the headers to have search bar

--- src/App.tsx
+++ src/App.tsx
@@ -95,124 +95,104 @@ function App() {
     }
   };
 
-  const [apiResult, setApiResult] = useState<any[]>([]);
-  const requestApi = async (e:any) => {
-    e.preventDefault();
-    try {
-        const res = await fetch(`api`, {
-        method: 'GET', 
-        headers: {
-          'Content/type' : "application/json"
-        }, 
-        body: JSON.stringify(formData)
-      })
-      const data = await res.json();
-      setApiResult(data); 
-    } catch (e) {
-      
-    }
-  }
-
   const toggleFilters = () => {
     setShowFilters(!showFilters);
   }
 
   return (
     <>
-    <div id='headers'>
-      <img src='' alt='logo'/>
-      <img src='' alt='user'/>
-    </div>
-
-    <div id='search'>
-      <img src='' alt='search'></img>
-      <button onClick={toggleFilters} id='toggleFilterBtn'>
-        {showFilters ? 'Hide Filters' : 'Show Filters'}
-      </button>
-    </div>
-    
-    <div id='main-content'>
-      {showFilters && (
-        <div className='filter'>
-          <form onSubmit={requestApi}>
-            {/* First row - Protein and Fat */}
-            <div className="input-row">
-              <label className='inputForm'>
-                Protein (g)
-                <input 
-                  type='number' 
-                  name='protein' 
-                  placeholder="0"
-                  value={inputValues.protein}
-                  onChange={handleInputChange}
-                  onBlur={handleBlur}
-                />
-              </label>
-              
-              <label className='inputForm'>
-                Fat (g)
-                <input 
-                  type='number' 
-                  name='fat' 
-                  placeholder="0"
-                  value={inputValues.fat}
-                  onChange={handleInputChange}
-                  onBlur={handleBlur}
-                />
-              </label>
-            </div>
-            
-            {/* Second row - Calories and Budget */}
-            <div className="input-row">
-              <label className='inputForm'>
-                Calories
-                <input 
-                  type='number' 
-                  name='calories' 
-                  placeholder="0"
-                  value={inputValues.calories}
-                  onChange={handleInputChange}
-                  onBlur={handleBlur}
-                />
-              </label>
-              
-              <label className='inputForm'>
-                Budget ($)
-                <input 
-                  type='number' 
-                  name='budget' 
-                  placeholder="0"
-                  value={inputValues.budget}
-                  onChange={handleInputChange}
-                  onBlur={handleBlur}
-                />
-              </label>
-            </div>
-            
-            {/* Button row */}
-            <div className="button-row">
-              <button id='buttonForm' type='submit'>Apply Filter</button>
-            </div>
-          </form>
-          
-          {/* Optional: Display current numeric values for debugging */}
-          <div className="debug-info">
-            Current: P:{formData.protein}g | F:{formData.fat}g | C:{formData.calories} | ${formData.budget}
-          </div>
+    <div id='root'>
+      <div id='header'>
+        <img src='' alt='logo'/>
+        <div id='searchBar'>
+          <img src='' alt='search' id='searchImg'/>
+          <input type='text' name='search picture' id='searchText' placeholder='Search Restaurant'/>
         </div>
-      )}
+        <button onClick={toggleFilters} id='toggleFilterBtn'>
+          {showFilters ? 'Hide Filters' : 'Show Filters'}
+        </button>
+      </div>
       
-      <div className='map'></div>
-    </div>
+      <div id='main-content'>
+        {showFilters && (
+          <div className='filter'>
+            <form onSubmit={requestApi}>
+              {/* First row - Protein and Fat */}
+              <div className="input-row">
+                <label className='inputForm'>
+                  Protein (g)
+                  <input 
+                    type='number' 
+                    name='protein' 
+                    placeholder="0"
+                    value={inputValues.protein}
+                    onChange={handleInputChange}
+                    onBlur={handleBlur}
+                  />
+                </label>
+                
+                <label className='inputForm'>
+                  Fat (g)
+                  <input 
+                    type='number' 
+                    name='fat' 
+                    placeholder="0"
+                    value={inputValues.fat}
+                    onChange={handleInputChange}
+                    onBlur={handleBlur}
+                  />
+                </label>
+              </div>
+              
+              {/* Second row - Calories and Budget */}
+              <div className="input-row">
+                <label className='inputForm'>
+                  Calories
+                  <input 
+                    type='number' 
+                    name='calories' 
+                    placeholder="0"
+                    value={inputValues.calories}
+                    onChange={handleInputChange}
+                    onBlur={handleBlur}
+                  />
+                </label>
+                
+                <label className='inputForm'>
+                  Budget ($)
+                  <input 
+                    type='number' 
+                    name='budget' 
+                    placeholder="0"
+                    value={inputValues.budget}
+                    onChange={handleInputChange}
+                    onBlur={handleBlur}
+                  />
+                </label>
+              </div>
+              
+              {/* Button row */}
+              <div className="button-row">
+                <button id='buttonForm' type='submit'>Apply Filter</button>
+              </div>
+            </form>
+            
+            {/* Optional: Display current numeric values for debugging */}
+            <div className="debug-info">
+              Current: P:{formData.protein}g | F:{formData.fat}g | C:{formData.calories} | ${formData.budget}
+            </div>
+          </div>
+        )}
+        
+        <div className='map'></div>
+      </div>
 
-    <div id='footer'>
-      <p>This work is licensed under <a href="https://creativecommons.org/licenses/by-nc/4.0/">CC BY-NC 4.0</a><img id='cc'src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" alt="CC"/><img id='cc' src="https://mirrors.creativecommons.org/presskit/icons/by.svg" alt="BY"/><img id='cc' src="https://mirrors.creativecommons.org/presskit/icons/nc.svg" alt="NC"/></p>
+      <div id='footer'>
+        <p>This work is licensed under <a href="https://creativecommons.org/licenses/by-nc/4.0/">CC BY-NC 4.0</a><img id='cc'src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" alt="CC"/><img id='cc' src="https://mirrors.creativecommons.org/presskit/icons/by.svg" alt="BY"/><img id='cc' src="https://mirrors.creativecommons.org/presskit/icons/nc.svg" alt="NC"/></p>
+      </div>
     </div>
-    
-    </div>
+    </>
   )
-
-  
 }
 
 
